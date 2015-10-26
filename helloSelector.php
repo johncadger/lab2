61607ae9e2d53a1e0e810fc77e4a06b31a448fb6
1204848 <j.c.1983@live.com>
2015-10-26 15:43:45 +0000

    a

---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<a href="http://1204848-lab1.azurewebsites.net/helloPrinter.php?query=earth">Blarg</a>
+<a href="http://1204848-lab1.azurewebsites.net/helloPrinter.php?query=mars">Blarg</a>
+<a href="http://1204848-lab1.azurewebsites.net/helloPrinter.php?query=uranus">Blarg</a>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1204848
+ * Date: 26/10/2015
+ * Time: 15:35
+ */
+
+$query = $_GET["query"];
