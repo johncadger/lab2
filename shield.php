af8443076aa4c50f7b07cfb2430a5207b2eb632e
1204848 <j.c.1983@live.com>
2015-10-26 16:16:26 +0000

    a

---
+++ shield.php
@@ -0,0 +1,17 @@
+
+<form action="shieldResponse.php" method="post">
+    <label>Forename<input type= text name=“forename”></label>
+    <label>Surname<input type= text name=“surname”></label>
+    <label>Date of Birth<input type=text name=“dob”></label>
+    <label>Gender<input type= text name=“gender”></label>
+</form>
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1204848
+ * Date: 26/10/2015
+ * Time: 16:05
+ */
---
+++ shieldResponse.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1204848
+ * Date: 26/10/2015
+ * Time: 16:09
+ */
+
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+
+echo $forename . $surname . $dob . $gender;
