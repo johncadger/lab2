855724426da0180882b62f63e8363b4a05c19339
1204848 <j.c.1983@live.com>
2015-12-07 15:21:13 +0000

    a

--- coursework/register.php
+++ coursework/register.php
@@ -31,7 +31,7 @@ include ("db_connect.php");
     $name = $_POST["name"];
 
     //Get userID.
-    $sql_query_getID = "SELECT COUNT(marvelMovieID) FROM marvelmovies";
+    $sql_query_getID = "SELECT * FROM marvelmovies";
     $result = $db->query($sql_query_getID);
     echo mysql_num_rows($result);
 
