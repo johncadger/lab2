e92817d795d8a4813ae91bcf2c58d188fe66aca5
johncadger <j.c.1983@live.com>
2015-12-09 12:33:25 +0000

    a

--- coursework/process.php
+++ coursework/process.php
@@ -15,6 +15,8 @@ $name = $_POST["name"];
 
 $query = $_GET['query'];
 
+$loginExists = 0;
+
 $sql_query_checkLogin = "SELECT * FROM marvelmovies where yearReleased = $login";
 $result = $db->query($sql_query_checkLogin);
 while($row = $result->fetch_array()){
