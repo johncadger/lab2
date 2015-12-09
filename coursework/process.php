3d39989667027f81dd9c39f3970ae574ba8d8bff
johncadger <j.c.1983@live.com>
2015-12-09 16:11:10 +0000

    a

--- coursework/process.php
+++ coursework/process.php
@@ -21,38 +21,4 @@ while($row = $result->fetch_array()){
     $loginExists++;
 }
 
-if ($query = "register"){
-    $country = $_POST["country"];
-    $name = $_POST["name"];
-
-    //Get userID.
-    $sql_query_getID = "SELECT * FROM marvelmovies";
-    $result = $db->query($sql_query_getID);
-    while($row = $result->fetch_array()){
-        $userID++;
-    }
-    $userID +=1;
-
-    if($loginExists > 0){
-        header('Location: register.php');
-    }
-    else{
-        //Insert information into database.
-        //$sql_query_insertUser = "INSERT INTO marvelmovies(marvelMovieID, yearReleased, title, productionStudio, notes) VALUES ($userID, $login, $password, $country, $name)";
-        //$db->query($sql_query_insertUser);
-        //$_SESSION['userID'] = $userID;
-    }
-}
-elseif($query = "login"){
-    if($loginExists > 0){
-        $sql_query_findID = "SELECT * FROM marvelmovies where yearReleased = $login";
-        $result = $db->query($sql_query_findID);
-        while($row = $sql_query_findID->fetch_array()) {
-            $_SESSION['userID'] = $row['marvelMovieID'];
-        }
-        header('Location: profile.php');
-    }
-    else{
-        header('Location: register.php');
-    }
-}
+echo $loginExists;
