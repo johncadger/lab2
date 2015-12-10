881bdc267743dbb48e2ec0dd60ab5d9894dafa42
johncadger <j.c.1983@live.com>
2015-12-10 16:40:15 +0000

    a

--- coursework/process.php
+++ coursework/process.php
@@ -21,42 +21,5 @@ while($row = $result->fetch_array()){
     $loginExists++;
 }
 
-if ($query = 'register'){
-    $country = $_POST["country"];
-    $name = $_POST["name"];
-
-    if($loginExists > 0){
-        echo "register"." ".$loginExists." ".$login." ".$query;
-        //header('Location: register.php');
-    }
-    else{
-        //Get userID.
-        $sql_query_getID = "SELECT * FROM Users where login LIKE '$login'";
-        $result = $db->query($sql_query_getID);
-        while($row = $result->fetch_array()){
-            $userID++;
-        }
-        $userID +=1;
-        $_SESSION['userID'] = $userID;
-        //Insert information into database.
-        //$sql_query_insertUser = "INSERT INTO marvelmovies(marvelMovieID, yearReleased, title, productionStudio, notes) VALUES ($userID, $login, $password, $country, $name)";
-        //$db->query($sql_query_insertUser);
-        echo $_SESSION['userID'];
-
-    }
-}
-elseif($query = 'login'){
-    if($loginExists > 0){
-        $sql_query_findID = "SELECT * FROM Users where login = '$login'";
-        $result = $db->query($sql_query_findID);
-        while($row = $sql_query_findID->fetch_array()) {
-            $_SESSION['userID'] = $row['user_id'];
-        }
-        header('Location: profile.php');
-    }
-    else{
-        echo $loginExists." "."login";
-        //header('Location: register.php');
-    }
-}
+echo $login." ".$loginExists." ".$query;
 
