365b3b431418b9d350bfd2fa3b41f96acfb3570d
johncadger <j.c.1983@live.com>
2016-01-12 12:05:41 +0000

    a

--- revision.php
+++ revision.php
@@ -5,20 +5,20 @@
  * Date: 12/01/2016
  * Time: 11:45
  */
-$juice = 'apple';
-echo "i drank some $juice juice<br />";
-echo 'i drank some $juice juice <br />';
+//$juice = 'apple';
+//echo "i drank some $juice juice<br />";
+//echo 'i drank some $juice juice <br />';
 
-$a = 0;
-$b = &$a;
-$b++;
-echo '$a == ', $a, '<br />';
+//$a = 0;
+//$b = &$a;
+//$b++;
+//echo '$a == ', $a, '<br />';
 
-//$arr = array('one','two','three','four','stop','five');
-//while (list(, $val) = each($arr)){
-//    if ($val == 'stop'){
-//        break 1;
-//    }
-//    echo "$val<br />\n";
-//}
+$arr = array('one','two','three','four','stop','five');
+while (list(, $val) = each($arr)){
+    if ($val == 'stop'){
+        break 1;
+    }
+    echo "$val<br />\n";
+}
 
