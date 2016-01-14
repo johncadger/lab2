fcce417fd109c47850393fa9f459731318620113
johncadger <j.c.1983@live.com>
2016-01-14 17:01:33 +0000

    a

--- revision.php
+++ revision.php
@@ -22,30 +22,30 @@
 //    echo "$val<br />\n";
 //}
 
-$days_of_week = array(
-    "monday", "tuesday", "wednesday", "thursday",
-    "friday", "saturday", "sunday"
-);
+//$days_of_week = array(
+//    "monday", "tuesday", "wednesday", "thursday",
+//    "friday", "saturday", "sunday"
+//);
 
-function shortenString($array){
-    foreach($array as $i){
-        echo substr($i,0,3);
-    }
-}
+//function shortenString($array){
+//    foreach($array as $i){
+//        echo substr($i,0,3);
+//    }
+//}
 
-shortenString($days_of_week);
+//shortenString($days_of_week);
 
 
-if (!isset($_COOKIE['count'])){
-    $cookie = 1;
-    setcookie('count',$cookie);
-}
-else{
-    $cookie = ++$_COOKIE['count'];
-    setcookie('count',$cookie);
-}
+//if (!isset($_COOKIE['count'])){
+//    $cookie = 1;
+//    setcookie('count',$cookie);
+//}
+//else{
+//    $cookie = ++$_COOKIE['count'];
+//    setcookie('count',$cookie);
+//}
 
-echo $_COOKIE['count'];
+//echo $_COOKIE['count'];
 
 $x = 1;
 if($x==1.0)
@@ -53,7 +53,24 @@ if($x==1.0)
 else
     echo "False <br />";
 $x = 1.0;
-if($x==1)
+if($x===1)
     echo "True <br />";
 else
     echo "False <br />";
+
+function name_of_coins($cents){
+    static $coins_of_usa = array(
+        1=>"penny", 5=>"nickel", 10=>"dime",
+        25=>"quarter", 50=>"half dollar", 100=>"dollar"
+    );
+    foreach($coins_of_usa as $i){
+        if($cents == $i)
+            $cents = $i;
+    }
+}
+
+$input = 5;
+
+name_of_coins($input);
+
+echo $input;
