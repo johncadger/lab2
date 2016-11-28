ae6e4fc40865ca7843f0f978e639c1e56d105f40
John Cadger <j.c.1983@live.com>
2016-11-28 17:53:49 +0000

    a

---
+++ blogsystem/inc/scripts/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelcrabb
+ * Date: 18/11/2015
+ * Time: 09:43
+ */
+
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "b8ed5106a0388c",
+    "54abfd2d",
+    "blogsystem"
+);
+
+// test if connection was established, and print any errors
+
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
+
