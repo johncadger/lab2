49266a83167fc1f81e69117e269dc16dce4d8ccc
John Cadger <j.c.1983@live.com>
2016-11-29 09:04:46 +0000

    a

--- test/profile.php
+++ test/profile.php
@@ -6,6 +6,7 @@
  * Time: 07:14
  */
 $username = $_GET["username"];
+$test = $_SESSION['username'];
 
 include("dbconnect.php");
 
@@ -13,4 +14,5 @@ $sql_query_name = "SELECT * FROM users1 where username = '$username'";
 $result = $db->query($sql_query_name);
 while($row = $result->fetch_array()){
     echo "<p>Name: ".$row['username'];
+    echo "<p>$test";
 }
