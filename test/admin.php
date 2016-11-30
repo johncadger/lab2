0e9408e93c342e737f747cccfbc0a0c5296f761e
John Cadger <j.c.1983@live.com>
2016-11-30 08:02:09 +0000

    a

--- test/admin.php
+++ test/admin.php
@@ -16,7 +16,9 @@ if (isset($_SESSION['admin'])){
         echo "
         <main>
             <p>Photographers to be approved:</p>
-        </main>
+            <form action=\"admin.php\" method=\"post\">
+                <select name='approve'>
+
         ";
 
         $sql = "SELECT * FROM users2 where type = 'photographer' and approved = false";
@@ -27,11 +29,19 @@ if (isset($_SESSION['admin'])){
             $ID = $row['ID'];
             $username = $row['username'];
 
-            echo "<li>{$username} - <a href='admin.php?approve=$ID'>Approve</a></li>";
-            //echo "Username: {$username} and ID: {$ID}";
+            //echo "<li>{$username} - <a href='admin.php?approve=$ID'>Approve</a></li>";
+            echo "<option value= {$ID}>{$username}</option>";
         }
 
         echo "
+                </select></br>
+                    <p><input type=\"submit\" value=\"Approve\"></p>
+            </form>
+        </main>
+
+
+
+
         <main>
             <p>All Users:</p>
         </main>
