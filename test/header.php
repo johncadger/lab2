0a6b9bf9a18bdc381b0186755033df431ada68ab
John Cadger <j.c.1983@live.com>
2016-11-30 11:07:30 +0000

    a

--- test/header.php
+++ test/header.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['photographer'])){
 
     //$username = $_SESSION['username'];
 
-    echo "<p><a href='profile.php?username='{$_SESSION['username']}>My Profile</a></p>";
+    echo "<p><a href='profile.php?username='.{$_SESSION['username']}>My Profile</a></p>";
 
     //echo "<li><a href=\"profile.php?username=". $row['username']."\">{$username}</a></li>";
     //echo "<li><a href='profile.php?username='{$username}>{$username}</a></li>";
