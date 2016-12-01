bffde70c8c942c4daf19df26fa6fcec74271ee85
John Cadger <j.c.1983@live.com>
2016-12-01 08:56:26 +0000

    a

--- test/add_photo.php
+++ test/add_photo.php
@@ -59,6 +59,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
     } else {
+
+        (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file));
+
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         } else {
