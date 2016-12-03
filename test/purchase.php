9d66d50e225dc8422301ed12dcbef3cdc2648073
John Cadger <j.c.1983@live.com>
2016-12-03 19:34:02 +0000

    a

--- test/purchase.php
+++ test/purchase.php
@@ -10,7 +10,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $ID = $_POST['purchaseID'];
 
-    echo $ID;
+    if(isset($_POST['ccard'])){
+
+        $sql = "SELECT * FROM photos WHERE ID = {$ID}";
+        $result = $db->query($sql);
+
+        while($row = $result->fetch_array()) {
+
+            echo"Congratulations, you've purchased {$row['title']}!";
+
+            echo"<img src={$row['URL']} id=\"purchase_image\"/>";
+        }
+
+
+
+
+
+
+    } else{
+
+        $sql = "SELECT * FROM photos WHERE ID = {$ID}";
+        $result = $db->query($sql);
+        while($row = $result->fetch_array()){
+
+            echo "
+        <section id='purchasePhoto'
+        <img src={$row['URL']} id=\"purchase_image\"/>
+        <p>Title: {$row['title']}</p>
+        <p>Price: £{$row['price']}</p>
+        </section>
+
+        <form action='purchase.php' method='post'>
+            <label><input type='text' name='ccard'</label>
+            <button name=\"purchaseID\" type=\"submit\" value=\"{$row['ID']}\">Confirm Purchase</button>
+        </form>
+        ";
+
+
+
+    }
+
+
+
+    }
 
 
 
